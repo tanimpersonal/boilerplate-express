ac8709286e89a69da5588fc462c33170f972ae7e
tanimpersonal <tashintanim2019@gmail.com>
2022-05-29 02:04:45 +0600

    mongo connected

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://dbuser1:89NuSGyMNpr6nn4o@cluster0.755op.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("connected");
+  // perform actions on the collection object
+  //   client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
