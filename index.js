567066f4e8c827bebc7351d3debb27e139271ce7
tanimpersonal <tashintanim2019@gmail.com>
2022-05-29 01:06:52 +0600

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+//middleware
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
